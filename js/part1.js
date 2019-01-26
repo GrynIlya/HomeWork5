744ff0331bce1f392253798c789f926d6e0957ab
Gryn Ilya <grynilia@gmail.com>
2019-01-26 23:21:36 +0200

    HomeWork(object)

--- js/part1.js
+++ js/part1.js
@@ -1,36 +1,124 @@
-'use strict';
+    'use strict';
+    var result={};
 
-let number1 = '11';
-let number2 = '99999999';
+    let password = prompt(`Введите ваш пароль:
+        Ваш пароль должен отвечать следующим требованиям:
+        1) Пароль должен содержать только буквенно-цифровые символы;
+        2) Длина пароля долдна быть минимум 6 символов,но не менее 20;
+        3) Пароль должен содержать буквы и цифры`, ``);
 
-let i;
-let arr=[];
-let maxLengs;
+    console.log(password);
 
-console.log('Узнаем сумму двух чисел ' + number1 + ' и ' + number2);
+    function validatePassword(password) {
 
-if(number1.length>number2.length){
-    maxLengs=number1.length;
-}else {
-    maxLengs=number2.length;
-}
-console.log(maxLengs);
+        if(password.length<6 || password.length>20) {
+            return 'invalid';
+        }
 
-for(i = 0;i < maxLengs;i++){
-    let numeral1=+number1.charAt(number1.length-i-1) || 0;
-    let numeral2=+number2.charAt(number2.length-i-1) || 0;
-    let bonus = arr[i] || 0;
-    console.log(numeral1);
-    console.log(numeral2);
-    arr[i]=numeral1 + numeral2+ bonus;
+        let i;
+        let letterAvailability;
+        let numberAvailability;
 
-    if(arr[i]>9){
-        arr[i]=arr[i]-10;
-        arr[i+1]=1;
+        for(i=0;i<password.length;i++){
+            let validateSymbol= password.charAt(i);
+            if(('Z'>= validateSymbol && validateSymbol >= 'A') || ('z'>= validateSymbol && validateSymbol >= 'a')){
+                letterAvailability = true;
+            }else if('0' <= validateSymbol && validateSymbol <= '9'){
+                numberAvailability = true;
+            }else{
+                return 'invalid';
+            }
+        }
+
+        if(numberAvailability===true && letterAvailability===true){
+            return 'valid';
+        }else{
+            return 'invalid';
+        }
+    }
+
+    result.validate=validatePassword (password);
+
+    if(result.validate=='invalid'){
+        console.log('пароль не соответствует требованиям')
+    }else{
+        let number = prompt(`Введите число:         
+       `, ``);
+
+        console.log(number);
+
+        checkNumber(number);
+        result.userNumber=checkNumber(number);
+    }
+
+    function checkNumber(number) {
+        let i;
+
+        if(number >= 2){
+            let check=true;
+            for(i = 2;i < number;i++){
+                if(number % i === 0){
+                    check=false;
+                }
+            }
+            if(check){
+                return true;
+            }
+        }
+
+        if (number % 2 == 0){
+            return true;
+        }
+
+        if (number % 10 == 0){
+            return true;
+        }
+        return false;
     }
 
 
-}
-console.log(arr.reverse());
+    if(result.userNumber==true){
+        let number1 = prompt(`Введите первое число:         
+       `, ``);
+        let number2 = prompt(`Введите второе число:         
+       `, ``);
+        getSum(number1,number2);
+    }
+    function getSum(number1,number2){
 
-console.log(arr.join(''));
+    let i;
+    let arr=[];
+    let maxLengs;
+    let sum;
+
+    console.log('Узнаем сумму двух чисел ' + number1 + ' и ' + number2);
+
+    if(number1.length>number2.length){
+        maxLengs=number1.length;
+    }else {
+        maxLengs=number2.length;
+    }
+    console.log(maxLengs);
+
+    for(i = 0;i < maxLengs;i++){
+        let numeral1=+number1.charAt(number1.length-i-1) || 0;
+        let numeral2=+number2.charAt(number2.length-i-1) || 0;
+        let bonus = arr[i] || 0;
+        console.log(numeral1);
+        console.log(numeral2);
+        arr[i]=numeral1 + numeral2+ bonus;
+
+        if(arr[i]>9){
+            arr[i]=arr[i]-10;
+            arr[i+1]=1;
+        }
+
+    }
+    arr.reverse();
+
+        sum=arr.join('');
+        console.log(sum);
+        result.sumNumbers=sum;
+    }
+
+    console.log(result);
